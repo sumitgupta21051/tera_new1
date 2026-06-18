56a30838f1218a6e11bee899159c6fe69e861094
unknown <sumitgupta21051@gmail.com>
2026-06-19 01:52:05 +0530

    jon1

--- main.tf
+++ main.tf
@@ -3,3 +3,19 @@ resource "azurerm_resource_group" "name" {
     location = "eastus"
   
 }
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.25.0"
+    }
+  }
+  
+
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "00442ed7-647a-4a64-8dae-fb9af5a63eff"
+}
